16d18aa750cf1d1331bb7b2adc70a3bbdadcdcbf
bishal-dhakal <bishaldhakal@yahoo.com>
2023-11-12 11:50:29 +0545

    created model

---
+++ firstapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    prduct_name = models.CharField(max_length=255)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.prduct_name
+    
+class Cart(models.Model):
+    cart_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    created_on = models.DateTimeField()
+
+class ProductInCart(models.Model):
+    class Meta:
+        unique_together=(('cart','product'),)
+    product_in_cart_id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+class Order(models.Model):
+    status_choices = (
+        (1,"Not Packed"),
+        (2,"Ready For Shipment"),
+        (3,"Shipped"),
+        (4,"Delivered")
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=status_choices,default=1)
