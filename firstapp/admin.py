023197fd58775b1dac70bf452dbbfb90d1ad7c4f
bishal-dhakal <bishaldhakal@yahoo.com>
2023-11-29 21:53:54 +0545

    created models and custom admin panel

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Register your models here.
 
@@ -7,3 +11,72 @@ admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(ProductInCart)
 admin.site.register(Order)
+admin.site.register(Deal)
+# admin.site.register(CustomUser)
+admin.site.register(Customer)
+admin.site.register(Seller)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email','is_staff','is_active')
+    list_filter = ('email','is_staff','is_active')
+    fieldsets = (
+        (None,{'fields':('email','password')}),
+        ('Permission',{'fields':('is_staff','is_active',"is_customer","is_seller")}),
+    )
+    add_fieldsets =(
+        (None,{
+            'classes':('wide',),
+            'fields':('email','password','password2','is_staff','is_active')
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+# admin.site.unregister(User)
+admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(User,UserAdmin)
+
+
+class ProductInCartInline(admin.TabularInline):
+    model = ProductInCart
+
+class CartInline(admin.TabularInline):
+    model = Cart
+
+class DealInline(admin.TabularInline):
+    model = Deal.user.through
+
+# class UserAdmin(UserAdmin):
+#     model = User
+#     list_display=('username','get_cart','is_staff','is_active')
+#     list_filter = ('username','is_staff','is_active')
+#     fieldsets = (
+#         (None,{'fields':('username','password')}),
+#         ('Permissions',{'fields':('is_staff',('is_active','is_superuser'),)}),
+#         ('Important dates',{'fields':('last_login','date_joined')}),
+#         ('Advanced options',{
+#             'classes':('collapse'),
+#             'fields':('groups','user_permissions'),
+#         })
+#     )
+#     add_fieldsets = (
+#         (None,{
+#             'classes':('wide'),
+#             'fields':('username','password','password2','is_staff','is_superuser','groups')
+#         })
+#     )
+#     inline= [
+#         CartInline,DealInline
+#     ]
+
+#     def get_cart(self,obj):
+#         return obj.cart
+#     search_fields=('username',)
+#     ordering= ('username',)
+
+
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,7 +1,70 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.contrib.auth.models import AbstractUser,PermissionsMixin ,AbstractBaseUser
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
+
+# class UserType(models.Model):
+#     CUSTOMER = 1
+#     SELLER = 2
+#     TYPE_CHOICES = (
+#         (SELLER,'SELLER'),
+#         (CUSTOMER,'CUSTOMER')
+#     )
+
+#     id = models.PositiveSmallIntegerField(choices=TYPE_CHOICES,primary_key=True)
+
+#     def __str__(self):
+#         return self.get_id_display()
+
+# class Customer(models.Model):
+#     user = models.OneToOneField(User, on_delete= models.CASCADE)
+#     address = models.CharField(max_length=1000)
+
+# class Seller(models.Model):
+#     user = models.OneToOneField(User, on_delete= models.CASCADE)
+#     tax = models.CharField()
+    
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    is_customer = models.BooleanField(default=True)
+    is_seller = models.BooleanField(default=False)
+
+
+    # type = (
+    #     (1,'Seller'),
+    #     (2,'Customer')
+    # )
+    # user_type = models.IntegerField(choices = type, default=1)
+
+    #user_type = models.ManyToManyField(UserType)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+    
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE)
+    address = models.CharField(max_length=10)
+
+
+class Seller(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE)
+    tax = models.CharField(max_length=10)
+    warehouse_loc = models.CharField(max_length=100)
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
@@ -11,9 +74,10 @@ class Product(models.Model):
     def __str__(self):
         return self.prduct_name
     
+    
 class Cart(models.Model):
     cart_id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_on = models.DateTimeField()
 
 class ProductInCart(models.Model):
@@ -31,5 +95,10 @@ class Order(models.Model):
         (3,"Shipped"),
         (4,"Delivered")
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     status = models.IntegerField(choices=status_choices,default=1)
+
+class Deal(models.Model):
+    user = models.ManyToManyField(CustomUser)
+    deal_name = models.CharField(max_length=255)
+
